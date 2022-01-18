425acfc97462633a71656b49962dca1fb0943981
Josef Karlsson <effektgubben@gmail.com>
2022-01-18 16:44:34 +0100

    Embarassing acceleration bug fixed

--- src/AnimatedEntity.ts
+++ src/AnimatedEntity.ts
@@ -6,8 +6,8 @@ class AnimatedEntity extends Entity {
 
     constructor(position: p5.Vector, size: p5.Vector, velocity: p5.Vector, acceleration: p5.Vector, fill: p5.Image | string, isSolid: boolean, damage: boolean) {
         super(position, size, fill, isSolid, damage);
-        this.velocity = new p5.Vector();
-        this.acceleration = new p5.Vector();
+        this.velocity = velocity;
+        this.acceleration = acceleration;
     }
     update(): void {
         this.velocity.add(this.acceleration);
