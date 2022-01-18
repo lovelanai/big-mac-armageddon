260c41de3079e2a8569b033d4e58826484e609c2
Love Lanai <love.lanai@medieinstitutet.se>
2022-01-18 16:15:38 +0100

    fixar arv till player från animatedentity

--- src/Player.ts
+++ src/Player.ts
@@ -1,46 +1,16 @@
-/** egenskaper på "player"
-* position, storlek & gravititet */
-const gravity = .8;
+/// <reference path ="AnimatedEntity.ts"/>
+class Player extends AnimatedEntity{
 
-
-class Player {
-
-    position: { x: number; y: number; };
-    velocity: { x: number; y: number; };
-    width: number;
-    height: number;
     constructor() {
-
-        this.position = {
-            x: 1100,
-            y: 0
-        }
-        this.velocity = {
-            x: 0,
-            y: 0,
-        }
-
-        this.width = 50;
-        this.height = 50;
-        
+        super(createVector(100, 0), createVector(50, 50), createVector(0, 0), createVector(0, 0.8), 'red', true, false )        
 
     }
+
     draw(): void {
-        rect(this.position.x, this.position.y, this.width, this.height)
+        rect(this.position.x, this.position.y, this.size.x, this.size.y)
         fill('red')
     }
 
-    update(): void {
-        this.draw()
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        if (this.position.y + this.height +
-            this.velocity.y <= height)
-            this.velocity.y += gravity;
-            else this.velocity.y = 0;
-
-    }
 }
 
     /** Stoppar rörelse till höger & vänster */
@@ -54,21 +24,21 @@ class Player {
     }
 
     const player = new Player();
-    function animate() {
-        requestAnimationFrame(animate);
-        let scrollOffset = 0;
+//     function animate() {
+//         requestAnimationFrame(animate);
+//         let scrollOffset = 0;
 
-        /** Gör att "player" rör sig vid knapptryck och stannar vid knapplyft */
-        if ((keys.right.pressed && player.position.x < 1100) || keys.right.pressed && scrollOffset === 0 && player.position.x > 0){
-            player.velocity.x = 8
-        } else if (keys.left.pressed && player.position.x > 650) {
-            player.velocity.x = -8
-        } else {
-            player.velocity.x = 0
-        }
-}
+//         /** Gör att "player" rör sig vid knapptryck och stannar vid knapplyft */
+//         if ((keys.right.pressed && player.position.x < 1100) || keys.right.pressed && scrollOffset === 0 && player.position.x > 0){
+//             player.velocity.x = 8
+//         } else if (keys.left.pressed && player.position.x > 650) {
+//             player.velocity.x = -8
+//         } else {
+//             player.velocity.x = 0
+//         }
+// }
 
-animate()
+// animate()
 
 
 
@@ -88,7 +58,6 @@ animate()
 
             case 87:
                 console.log('up')
-                if (player.velocity.y === 0) player.velocity.y -= 20
                 break
         }
 
