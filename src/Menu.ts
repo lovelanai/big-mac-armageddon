951a559dc04d25c3ea41180b981c3f1cc0b3d2c3
Prisberg <fille.risberg@live.se>
2022-01-26 10:16:09 +0100

    update menu

--- src/Menu.ts
+++ src/Menu.ts
@@ -7,7 +7,7 @@ class Menu implements Visual {
     constructor(message: string) {
         this.message = message;
     }
-    
+
     update(): void {
         if (keyCode === ENTER) {
             console.log('game init')
@@ -18,28 +18,28 @@ class Menu implements Visual {
     draw(): void {
         //Menu text
         background(222, 6, 18)
+        image(images.ronaldMenu, 750, 485)
         fill(241, 163, 10)
         textAlign(CENTER)
         textSize(110)
         textFont(fonts.mcLawsuit)
-        text('M', 680, 150)
+        text('M', 680, 200)
         textSize(100)
         textFont(fonts.roboto)
-        text('Big      ac', 640, 150)
+        text('Big      ac', 640, 200)
         textSize(70)
         textFont(fonts.pressStart2p)
         fill(43, 69, 147)
-        text('Armageddon', 648, 240)
+        text('Armageddon', 648, 290)
         fill(241, 163, 10)
-        text('Armageddon', 640, 240)
+        text('Armageddon', 640, 290)
 
         if (frameCount % 100 < 30) {
             fill(241, 163, 10, 0)
         } else {
             fill(241, 163, 10)
             textSize(40)
-            text(`${this.message}`, 640, 360);
+            text(`${this.message}`, 640, 450);
         }
-
     }
 }
